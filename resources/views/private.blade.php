229abb10dbf61149474c4fff6dbfc9e51e373d2e
adds111 <mr.mikhail.ya@gmail.com>
2023-10-16 16:36:16 +0300

    hi1

--- resources/views/private.blade.php
+++ resources/views/private.blade.php
@@ -13,16 +13,6 @@
             @endforeach
         </ul>
     </div>
-        {{--    <form  method="post" action="/fileup" enctype="multipart/form-data">--}}
-        {{--        @csrf--}}
-        {{--        <div class="input-group row mt-3">--}}
-        {{--            <div class="col">--}}
-        {{--            <label for="file1" class="form-label">Выберете фил для загрузки(не более 5м)</label>--}}
-        {{--            <input type="file" name="file1" id="file1" class="form-control">--}}
-        {{--<input type="submit" class="btn btn-primary">--}}
-        {{--            </div>--}}
-        {{--        </div>--}}
-        {{--    </form>--}}
     <a class="btn btn-primary my-3" href="/logout">Назад</a>
     <button class="arr btn btn-primary n">
         <img src="{{asset('img/arrow-right_icon-icons.com_72375.svg')}}" height="21" width="38">
